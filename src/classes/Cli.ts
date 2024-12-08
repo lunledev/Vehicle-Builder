a1bf9889e81db97267b37525a523ff0ca27fa229
lunledev <183235731+lunledev@users.noreply.github.com>
2024-12-08 13:33:31 -0800

    minor changes to then of Motorbike in Cli_tsfile

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -254,6 +254,17 @@ class Cli {
       ])
       .then((answers) => {
         // TODO: Use the answers object to pass the required properties to the Motorbike constructor
+        const motorbike = new Motorbike(
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          answers.year,
+          answers.weight,
+          answers.topSpeed,
+          []
+        );
+
         // TODO: push the motorbike to the vehicles array
         // TODO: set the selectedVehicleVin to the vin of the motorbike
         // TODO: perform actions on the motorbike
