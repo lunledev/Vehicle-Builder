bf5582a5e2a5f3fcba6075e66e9dc0539ef75fdb
lunledev <183235731+lunledev@users.noreply.github.com>
2024-12-08 10:18:07 -0800

    intial setup of printDetails of Motorbikeclass

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -64,9 +64,14 @@ class Motorbike extends Vehicle{
     }
 
   // TODO: Override the printDetails method from the Vehicle class
-  // TODO: The method should call the printDetails method of the parent class
+  override printDetails(): void {
+   // TODO: The method should call the printDetails method of the parent class
   // TODO: The method should log the details of the Motorbike
   // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+      
+
+  }
+ 
 }
 
 // Export the Motorbike class as the default export
